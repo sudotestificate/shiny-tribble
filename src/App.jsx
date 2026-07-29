201ba259f70f8fc9a659bad2c0237374d6feb143
Toast (gastown) <Toast@gastown.local>
2026-07-29 14:52:45 +0000

    feat: add preview route to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './components/Layout';
+import Dashboard from './views/Dashboard';
+import Transactions from './views/Transactions';
+import Accounts from './views/Accounts';
+import Journals from './views/Journals';
+import Preview from './views/Preview';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Navigate to="/dashboard" replace />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="transactions" element={<Transactions />} />
+        <Route path="accounts" element={<Accounts />} />
+        <Route path="journals" element={<Journals />} />
+        <Route path="preview" element={<Preview />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
